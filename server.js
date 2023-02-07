b5afbc5eafb2e5adff318c61168262ee2cd27894
Juan Carlos Brun <juancarlosbrun10@gmail.com>
2023-02-07 16:59:22 +0100

    Cambio de orden: Cors, Json y Routes

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const { application } = require('express');
 const express = require('express');
 const bp = require('body-parser')
 
+// Se importa desde .env para no compartir credenciales
+// (y si no existe, para este caso se tomará los datos directamente, que es inseguro)
+const PORT = process.env.PORT // || 5000;
+
+// ----- MIDDLEWARE
+// Todo lo que va dentro de use sería un middleware
+// Archivos estáticos. Deberás crear un archivo public/index.html para ver el resultado
+app.use(express.static('public')); // Archivos estáticos colgarán de /public/
+app.use(express.json()); // Soporte de JSON
 
 const app = express();
 app.use(bp.json())
@@ -13,18 +22,6 @@ app.use(bp.urlencoded({ extended: true }))
 const routes = require('./routes');
 // Rutas
 app.use('/api', routes);
-
-// Se importa desde .env para no compartir credenciales
-// (y si no existe, para este caso se tomará los datos directamente, que es inseguro)
-const PORT = process.env.PORT // || 5000;
-
-
-// ----- MIDDLEWARE
-// Todo lo que va dentro de use sería un middleware
-// Archivos estáticos. Deberás crear un archivo public/index.html para ver el resultado
-app.use(express.static('public')); // Archivos estáticos colgarán de /public/
-app.use(express.json()); // Soporte de JSON
-
 
 // ----- INICIA SERVIDOR
 // Inicia Servidor en puerto de IP:3000
